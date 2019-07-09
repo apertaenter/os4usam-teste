0bc296979bdd6475d374eb1c62052263249f046b
Aperta Enter <apertaenter@gmail.com>
2019-07-09 15:08:48 -0300

    GET included

--- src/main/java/com/herokuapp/os4usamteste/controller/ContatoController.java
+++ src/main/java/com/herokuapp/os4usamteste/controller/ContatoController.java
@@ -1,9 +1,11 @@
 package com.herokuapp.os4usamteste.controller;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,13 +36,28 @@ public class ContatoController {
 		return contato;
 	}
 	
+	private Collection<Contato> listar(){
+		if (contatos == null) {
+			contatos = new HashMap<>();
+		}
+		return contatos.values();
+	}
+	
 	// End points
 	
-	@RequestMapping(method=RequestMethod.POST,value="/contatos")
+	@RequestMapping(method=RequestMethod.POST,value="/contatos",consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Contato> incluirContato(@RequestBody Contato contato) {
 		
 		Contato contatoIncluido = incluir(contato);
 		
 		return new ResponseEntity<Contato>(contatoIncluido,HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/contatos",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Contato>> listarContatos() {
+		
+		Collection<Contato> listaDeContatos = listar();
+		
+		return new ResponseEntity<>(listaDeContatos,HttpStatus.OK);
 	}
 }
